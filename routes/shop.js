78ad75d23bed0a8d02398663a03ba2e13ce14849
sachin barali <baralisachin472@gmail.com>
2025-12-08 23:32:50 +0545

    middleware added and route protected

--- routes/shop.js
+++ routes/shop.js
@@ -1,25 +1,27 @@
-const express = require('express');
+const express = require("express");
 
-const shopController = require('../controllers/shop');
+const shopController = require("../controllers/shop");
 
 const router = express.Router();
 
-router.get('/', shopController.getIndex);
+const isAuth = require("../middleware/is-auth");
 
-router.get('/products', shopController.getProducts);
+router.get("/", shopController.getIndex);
 
-router.get('/products/:productId', shopController.getProduct);
+router.get("/products", shopController.getProducts);
 
-router.get('/cart', shopController.getCart);
+router.get("/products/:productId", shopController.getProduct);
 
-router.post('/cart', shopController.postCart);
+router.get("/cart", isAuth, shopController.getCart);
 
-router.post('/cart-delete-item', shopController.postCartDeleteProduct);
+router.post("/cart", isAuth, shopController.postCart);
 
-router.post('/create-order',shopController.postOrder);
+router.post("/cart-delete-item", isAuth, shopController.postCartDeleteProduct);
 
-router.get('/orders', shopController.getOrders);
+router.post("/create-order", isAuth, shopController.postOrder);
 
-router.get('/checkout', shopController.getCheckout);
+router.get("/orders", isAuth, shopController.getOrders);
+
+router.get("/checkout", isAuth, shopController.getCheckout);
 
 module.exports = router;
