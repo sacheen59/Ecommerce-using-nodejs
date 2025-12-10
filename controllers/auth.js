3ac091382f420dc96f95bd6f6e6c537302652366
sachin barali <baralisachin472@gmail.com>
2025-12-10 09:33:14 +0545

    showing error message during signup and login using flash message

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,17 +2,30 @@ const bcrypt = require("bcryptjs");
 const User = require("../models/user");
 
 exports.getLogin = (req, res, next) => {
-  //   const isLoggedIn = req.get("Cookie").split(";")[3].trim().split("=")[1];
+  let message = req.flash("error");
+  if (message.length > 0) {
+    message = message;
+  } else {
+    message = null;
+  }
   res.render("auth/login", {
     path: "/login",
     pageTitle: "Login",
+    errorMessage: message,
   });
 };
 
 exports.getSignup = (req, res, next) => {
+  let message = req.flash("error");
+  if (message.length > 0) {
+    message = message;
+  } else {
+    message = null;
+  }
   res.render("auth/signup", {
     path: "/signup",
     pageTitle: "signup",
+    errorMessage: message,
   });
 };
 
@@ -24,6 +37,7 @@ exports.postLogin = (req, res, next) => {
   User.findOne({ email: email })
     .then((user) => {
       if (!user) {
+        req.flash("error", "Invalid email or password.");
         return res.redirect("/login");
       }
       bcrypt.compare(password, user.password).then((doMatch) => {
@@ -50,6 +64,7 @@ exports.postSignup = (req, res, next) => {
   User.findOne({ email: email })
     .then((userDoc) => {
       if (userDoc) {
+        req.flash("error", "Email already exists, please try another email.");
         return res.redirect("/signup");
       }
       //hashed the password
