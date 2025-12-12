65096e065f795a4a70bd51d37263d15b086b1ef8
sachin barali <baralisachin472@gmail.com>
2025-12-12 09:52:43 +0545

    adding validation using express validator

--- controllers/auth.js
+++ controllers/auth.js
@@ -3,6 +3,7 @@ const crypto = require("crypto");
 
 const User = require("../models/user");
 const resend = require("../util/resend_email");
+const { validationResult } = require("express-validator");
 
 exports.getLogin = (req, res, next) => {
   let message = req.flash("error");
@@ -63,6 +64,15 @@ exports.postSignup = (req, res, next) => {
   const email = req.body.email;
   const password = req.body.password;
   const confirmPassword = req.body.confirmPassword;
+  const errors = validationResult(req);
+
+  if (!errors.isEmpty()) {
+    return res.status(422).render("auth/signup", {
+      path: "/signup",
+      pageTitle: "signup",
+      errorMessage: errors.array()[0].msg,
+    });
+  }
   //check whether the user with email already exists
   User.findOne({ email: email })
     .then((userDoc) => {
--- routes/auth.js
+++ routes/auth.js
@@ -1,4 +1,6 @@
 const express = require("express");
+const { check, body } = require("express-validator");
+
 const router = express.Router();
 
 const authController = require("../controllers/auth");
@@ -9,7 +11,32 @@ router.get("/signup", authController.getSignup);
 
 router.post("/login", authController.postLogin);
 
-router.post("/signup", authController.postSignup);
+router.post(
+  "/signup",
+  [
+    check("email")
+      .isEmail()
+      .withMessage("Please enter a valid email.")
+      .custom((value, { req }) => {
+        if (value === "test@test.com") {
+          throw new Error("This email is forbidden.");
+        }
+        return true;
+      }),
+    body(
+      "password",
+      "please enter a password with only numbers and text and at least 5 characters."
+    )
+      .isLength({ min: 6 })
+      .isAlphanumeric(),
+    body("confirmPassword").custom((value, { req }) => {
+      if (value !== req.body.password) {
+        throw new Error("Password and Confirm password must be same.");
+      }
+    }),
+  ],
+  authController.postSignup
+);
 
 router.post("/logout", authController.postLogout);
 
